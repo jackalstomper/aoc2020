171d2bf31afad453ad205c3551595aa35251ceba
icefire <jackalstomper@users.noreply.github.com>
2020-12-03 01:26:09 -0500

    Make Day3 faster

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,53 +1,70 @@
+#![feature(test)]
+
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::{self, BufRead};
 
-fn main() {
-    let forest: Vec<Vec<bool>> = {
-        let file = File::open("input.txt").unwrap();
-        io::BufReader::new(file)
-            .lines()
-            .map(|line| line.unwrap())
-            .filter(|line| !line.is_empty())
-            .map(|line| line.chars().map(|c| c == '#').collect())
-            .collect()
-    };
+extern crate test;
+use test::Bencher;
 
+fn main() {
+    do_stuff(read_file().as_bytes());
+}
+
+fn read_file() -> String {
+    let mut s = String::new();
+    File::open("input.txt")
+        .unwrap()
+        .read_to_string(&mut s)
+        .unwrap();
+    s
+}
+
+fn do_stuff(file_content: &[u8]) {
     //part 1
-    println!("Part1 hit count: {}", get_hits(&forest, 3, 1));
+    println!("Part1 hit count: {}", get_hits(file_content, 3, 1));
 
     // part 2
     println!(
         "Part2 hit count: {}",
-        get_hits(&forest, 1, 1)
-            * get_hits(&forest, 3, 1)
-            * get_hits(&forest, 5, 1)
-            * get_hits(&forest, 7, 1)
-            * get_hits(&forest, 1, 2)
+        get_hits(file_content, 1, 1)
+            * get_hits(file_content, 3, 1)
+            * get_hits(file_content, 5, 1)
+            * get_hits(file_content, 7, 1)
+            * get_hits(file_content, 1, 2)
     );
 }
 
-fn get_hits(forest: &Vec<Vec<bool>>, right_step: usize, down_step: usize) -> usize {
+fn get_hits(forest: &[u8], right_step: usize, down_step: usize) -> usize {
+    let width = 31;
     let mut hit_count = 0;
     let mut x = 0usize;
     let mut y = 0usize;
     loop {
-        if y >= forest.len() {
+        if y + x >= forest.len() {
             // complete
             break;
         }
 
-        if x >= forest[y].len() {
-            // wrap around
-            x = x - forest[y].len();
+        if x >= width {
+            x = x - width;
         }
 
-        if forest[y][x] {
+        if forest[y + x] == 35 {
             hit_count += 1;
         }
 
-        y += down_step;
         x += right_step;
+        y += down_step * (width + 1); // + 1 to skip newline character
     }
 
     hit_count
+}
+
+#[bench]
+fn bench(b: &mut Bencher) {
+    let content = read_file();
+    b.iter(|| {
+        do_stuff(content.as_bytes());
+    });
 }
